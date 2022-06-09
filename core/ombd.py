e0003270f02495c79f7c7021cc92c66669b10043
karthikeyan_01 <karthikerathore@gmail.com>
2022-06-10 01:33:37 +0530

    indetation error:geany

--- core/ombd.py
+++ core/ombd.py
@@ -12,50 +12,51 @@ movie_info = {
 'imdbID': None,
 'type': None,
 'poster_url': None,
+
 }
 
 
 
 def get_by_name(movie):
-	API = 'https://www.omdbapi.com/'
-	params = (('s', movie), ('apikey', API_KEY))
-	response = requests.get(API, params=params)  
-	error = None
-	
-	if response.status_code == 200:
-		# replace logger
-		print('API is working')
-	else:
-		error = "API is not Working"
-		# print(error)
-	
-	movies = []
-	if error is None:
-		data = response.json()
-		if data['Response'] == 'False':
-			error = data['Error']
-			return movies, error
-		for i in range (0, len(data['Search'])):
-			movie_info['title'] = data['Search'][i]['Title']
-			movie_info['year'] = data['Search'][i]['Year']
-			movie_info['imdbID'] = data['Search'][i]['imdbID']
-			movie_info['type'] = data['Search'][i]['Type']
-			movie_info['poster_url'] = data['Search'][i]['Poster']
-			# print(movie_info)
-			movies.append(movie_info)
-		return movies, error
-		 
-			
+  API = 'https://www.omdbapi.com/'
+  params = (('s', movie), ('apikey', API_KEY))
+  response = requests.get(API, params=params)  
+  error = None
+  
+  if response.status_code == 200:
+    # replace logger
+    print('API is working')
+  else:
+    error = "API is not Working"
+    # print(error)
+  
+  movies = []
+  if error is None:
+    data = response.json()
+    if data['Response'] == 'False':
+      error = data['Error']
+      return movies, error
+    for i in range (0, len(data['Search'])):
+      movie_info['title'] = data['Search'][i]['Title']
+      movie_info['year'] = data['Search'][i]['Year']
+      movie_info['imdbID'] = data['Search'][i]['imdbID']
+      movie_info['type'] = data['Search'][i]['Type']
+      movie_info['poster_url'] = data['Search'][i]['Poster']
+      # print(movie_info)
+      movies.append(movie_info)
+    return movies, error
+     
+      
 
 def get_movie(movie, apply_filter):
-	# get by name
-	if apply_filter == 'NAME':
-		movies = get_by_name(movie)
-		return movies
-	
-	# get by id
-	elif apply_filter == 'ID':
-		pass
-	# invalid filter
-	else:
-		pass
+  # get by name
+  if apply_filter == 'NAME':
+    movies = get_by_name(movie)
+    return movies
+  
+  # get by id
+  elif apply_filter == 'ID':
+    pass
+  # invalid filter
+  else:
+    pass
